19f9ed8b46049c0962be0dd872983f4bc4dc15c4
Arunaraj <ashveena_arunaraj@optum.com>
2024-05-22 18:18:22 +0530

    promiseapi

--- asyncawait.js
+++ asyncawait.js
@@ -1,25 +1,87 @@
-//returning a value like string or array
-async function getData(){
-    return "Namaste";
+//async and await combo is used to handle promises in a more elegant way
 
-}
-const data=getData();
-console.log(data);//returns only a promise
+//before async await how promises were handled
 
-//if you want the data back you can use the promisd.then function
-data.then(res=>console.log(res));
-
-//returning a promise
-const p=new Promise((resolve,reject)=>{
-    resolve("Promise reoslved")
+//promise
+/*const pr=new Promise((resolve)=>{
+    setTimeout(()=>resolve("promise resolved"),1000);
 });
 
-async function getData(){
-    return p;
+ function getData(){
+    pr.then(res=>console.log(res));
+    console.log("hello");
+   
+
+
+ }
+
+
+ getData();*/
+
+
+ //how async function handles promises
+
+ const pro=new Promise((resolve)=>{
+    setTimeout(()=>resolve("promise resolved"),10000);
+   });
+
+  const pr=new Promise((resolve)=>{
+    setTimeout(()=>resolve("promise resolved"),20000);
+   });
+
+
+   const prom=new Promise((resolve)=>{
+    setTimeout(()=>resolve("promise resolved"),12000);
+   });
+
+    async function getDataAsync(){
+        console.log("hello- world");
+
+        const val4=await pro;
+        console.log( val4);
+       // console.log("hello"); 
+
+        const val=await pr;
+        console.log( val);
+        //console.log("hello");
+
+        //when 2 await lines are there then the second await line and first await line 
+        //will simultaneously wait for only 10 seconds then all will be printed
+
+      /*  const val2=await pr;
+        console.log( val2);
+        console.log("hello");*/
+
+       /* const val3=await prom;
+        console.log( val3);
+        console.log("hello");*/
+
+        
+    }
+
+    getDataAsync();
+    
+
+//difference between handling promises normally and using async await
+//in async await js engine wait for the promise to be resolved before going to next line 
+//which is not possible in case of normal way of handling promises
+
+//in certain cases promises continue from the point/time it left in the previous exeution
+
+
+//real world example
+//fetching data from api
+
+const GITHUB_API = "https://api.github.com/users/Ashveena";
+
+async function fetchData(){
+    const data=await fetch(GITHUB_API);
+
 }
 
-const dataprom=getData();
-console.log(dataprom);//returns only a promise
+fetchData();
 
-//if you want the data back you can use the promisd.then function
-dataprom.then(res=>console.log(res));
+
+
+
+
--- promiseapi.js
+++ promiseapi.js
@@ -1,9 +1,109 @@
+//lingos famous in promise world
+//resolve reject success failure fulfilled  rejected
+//settled-got the result
+
+
+//promiseall
+/*const p1= new Promise((resolve,reject)=>
+{
+    setTimeout(()=>resolve("Async operation 1"),3000);
+})
+
+const p2= new Promise((resolve,reject)=>
+    {
+        //setTimeout(()=>resolve("Async operation 2"),1000);
+        setTimeout(()=>reject("Async operation 2"),1000);
+    })
+
+    const p3= new Promise((resolve,reject)=>
+        {
+            setTimeout(()=>resolve("Async operation 3"),2000);
+        });
+
+
+Promise.all([p1,p2,p3])
+.then(res=>
+    {console.log(res)})
+.catch(err=>{console.error(err)}
+);*/
+ 
+//promiseallsettled
+/*const p1= new Promise((resolve,reject)=>
+    {
+        setTimeout(()=>resolve("Async operation 1"),3000);
+    })
+    
+    const p2= new Promise((resolve,reject)=>
+        {
+            setTimeout(()=>resolve("Async operation 2"),1000);
+            //setTimeout(()=>reject("Async operation 2"),1000);
+        })
+    
+        const p3= new Promise((resolve,reject)=>
+            {
+                setTimeout(()=>resolve("Async operation 3"),2000);
+            });
+    
+    
+    Promise.allSettled([p1,p2,p3])
+    .then(res=>
+        {console.log(res)})
+    .catch(err=>{console.error(err)}
+    );*/
+
+    //promiserace
+//promiseallsettled
+/*const p1= new Promise((resolve,reject)=>
+    {
+        setTimeout(()=>resolve("Async operation 1"),3000);
+    })
+    
+    const p2= new Promise((resolve,reject)=>
+        {
+           setTimeout(()=>resolve("Async operation 2"),1000);
+           // setTimeout(()=>reject("Async operation 2"),1000);
+        })
+    
+        const p3= new Promise((resolve,reject)=>
+            {
+                setTimeout(()=>reject("Async operation 3"),2000);
+            });
+    
+    
+    Promise.race([p1,p2,p3])
+    .then(res=>
+        {console.log(res)})
+    .catch(err=>{console.error(err)}
+    );*/
+
+    //promiseany
+    //promiseallsettled
 const p1= new Promise((resolve,reject)=>
     {
-   resolve("success")
-});
+        setTimeout(()=>reject("Async operation 1"),3000);
+    })
+    
+    const p2= new Promise((resolve,reject)=>
+        {
+           //setTimeout(()=>resolve("Async operation 2"),1000);
+            setTimeout(()=>reject("Async operation 2"),1000);
+        })
+    
+        const p3= new Promise((resolve,reject)=>
+            {
+                setTimeout(()=>reject("Async operation 3"),2000);
+            });
+    
+    
+    Promise.any([p1,p2,p3])
+    .then(res=>
+        {console.log(res)})
+    .catch(err=>{console.error(err);
+        console.log(err.errors);
+    }
+    )
+    ;
 
 
 
-Promise.all([p1,p2,p3]);
- 
+
